691d2ae0cf27139914e0afc01ea48dc0942d018d
Markus Kahl <machisuji@googlemail.com>
2011-01-21 22:25:31 +0100

    Added scalatest to project dependencies.

--- project/build/SquerylProject.scala
+++ project/build/SquerylProject.scala
@@ -47,5 +47,9 @@ class SquerylProject(info: ProjectInfo) extends DefaultProject(info) {
   
   //val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "provided"
   
-  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "provided"  
+  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "provided"
+  
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+  val scalatest = "org.scalatest" % "scalatest" % "1.2.1-SNAPSHOT" % "test"
 }
+
